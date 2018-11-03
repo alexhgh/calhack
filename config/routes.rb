1f8e5d88820d2765494686a23363b5686cb48ee1
Alex Ho <hogunghiu@msn.com>
2018-11-03 19:09:36 +0000

    devise installation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+  
   get 'welcome/index'
   
   root 'welcome#index'
