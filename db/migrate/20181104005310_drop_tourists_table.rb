74023d58d87ea93a911e53e1bce57f4e13202ec6
Gilbert Lo <lhygilbert@gmail.com>
2018-11-03 17:57:37 -0700

    drop table

--- db/migrate/20181104005310_drop_tourists_table.rb
+++ db/migrate/20181104005310_drop_tourists_table.rb
@@ -1,4 +1,5 @@
 class DropTouristsTable < ActiveRecord::Migration[5.0]
   def change
+    drop_table :tourists
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -35,13 +35,6 @@ ActiveRecord::Schema.define(version: 20181104005310) do
     t.integer  "user_id"
   end
 
-  create_table "tourists", force: :cascade do |t|
-    t.text     "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
